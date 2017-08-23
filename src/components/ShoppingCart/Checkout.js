46bf5eb1669ea09e5c6da041a99fba434999360e
Joe Isabell <joeisabell@me.com>
2017-08-23 12:39:22 -0600

    added basic stripe checkout, still needs customized

--- src/components/ShoppingCart/Checkout.js
+++ src/components/ShoppingCart/Checkout.js
@@ -1,21 +1,36 @@
 import React from 'react';
 import styled from 'styled-components';
+import StripeCheckout from 'react-stripe-checkout';
+
 
 const Checkout = () => {
-    return (
-        <Button>Proceed to checkout</Button>
-    );
-};
+  const onRecieveToken = token => {
+    console.log('I got this token back:', token)
+  }
+  return (
+    <StripeCheckout
+      stripeKey={process.env.REACT_APP_STRIPE_PUBLIC_KEY}
+      token={onRecieveToken}
+      ComponentClass={Button}
+    >
+    Checkout
+  </StripeCheckout>
+  )
+}
 
 export default Checkout;
 
 const Button = styled.button`
-    ${props => props.theme.secondaryFont({})}
-    background-color: ${props => props.theme.main};
-    text-transform: uppercase;
-    border: none;
-    color: white;
-    height: 70px;
-    font-size: 32px;
-    letter-spacing: 1px;
+  ${props => props.theme.secondaryFont({})}
+  background-color: ${props => props.theme.main};
+  text-transform: uppercase;
+  border: none;
+  color: white;
+  height: 70px;
+  font-size: 32px;
+  letter-spacing: 1px;
+  cursor: pointer;
+  &:hover {
+    background: #B20E0D;
+  }
 `
